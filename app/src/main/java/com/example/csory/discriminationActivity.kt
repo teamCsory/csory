4c1c97c423a22f3d89c54795ffa7fc24fb7b234f
teamCsory <teamCsory@gmail.com>
2022-04-03 23:17:31 +0900

    k

--- app/src/main/java/com/example/csory/discriminationActivity.kt
+++ app/src/main/java/com/example/csory/discriminationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.csory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_discrimination.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class discriminationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_discrimination)
+
+        btn_phoneme.setOnClickListener{
+            val myIntent = Intent(this, phonemeActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/csory/phonemeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.csory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class phonemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phoneme)
+    }
+}
