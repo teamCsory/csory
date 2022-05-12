7d7306729d8754025b255d4e23a5673e073b6713
teamCsory <teamCsory@gmail.com>
2022-05-12 23:26:59 +0900

    ㅇ

--- app/src/main/java/com/example/csory/FunctionActivity.kt
+++ app/src/main/java/com/example/csory/FunctionActivity.kt
@@ -1,11 +1,18 @@
 package com.example.csory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_function.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FunctionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_function)
+        start.setOnClickListener{
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
