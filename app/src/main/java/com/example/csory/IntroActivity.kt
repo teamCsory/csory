71cbc0837018ed8ac097fd7765cc5f3fdd081696
teamCsory <teamCsory@gmail.com>
2022-05-12 20:14:57 +0900

    z

--- app/src/main/java/com/example/csory/IntroActivity.kt
+++ app/src/main/java/com/example/csory/IntroActivity.kt
@@ -15,7 +15,7 @@ class IntroActivity : AppCompatActivity() {
         var handler = Handler()
         handler.postDelayed({var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }, 1000)
+        }, 3000)
     }
 
     override fun onPause() {
