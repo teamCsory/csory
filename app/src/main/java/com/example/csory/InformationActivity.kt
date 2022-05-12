14f688fb4322d94078c06f98052687737c2e00f3
teamCsory <teamCsory@gmail.com>
2022-05-12 22:54:44 +0900

    ㅇ

--- app/src/main/java/com/example/csory/InformationActivity.kt
+++ app/src/main/java/com/example/csory/InformationActivity.kt
@@ -1,11 +1,19 @@
 package com.example.csory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_information.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class InformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_information)
+
+        greennext.setOnClickListener {
+            val myIntent = Intent(this, FunctionActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
